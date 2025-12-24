ec125c7a66543daa5d0788019f761e3a39b916f8
sonukr108 <sv808494@gmail.com>
2025-12-24 14:10:26 +0530

    Some changes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 
-const URL = require("../models/url");
 const urlRoute = require("../routes/url");
 const { connectToMongoDB } = require("./connnect");
 
@@ -13,14 +11,11 @@ connectToMongoDB(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected."))
   .catch((err) => console.error("MongoDB Error:", err));
 
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 
-app.get("/test", async (req, res) => {
-  const allUrls = await URL.find({});
-  return res.render("home.ejs", { urls: allUrls });
+app.get("/", async (req, res) => {
+  return res.end("Welcome in home page");
 });
 
 app.use("/url", urlRoute);
